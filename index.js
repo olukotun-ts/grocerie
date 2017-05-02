30f63798ba36fc2817621cd3d6f0f0d01004dd67
Sam Olukotun <otsam88@gmail.com>
2017-05-02 10:44:04 -0700

    Connects to db.

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 // Dependencies
 // ============
 var express = require('express')
+var mongoose = require('mongoose')
 var parser = require('body-parser')
 
+// Fire up database.
+// =================
+mongoose.connect('mongodb://localhost/groceriedb')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'Error connecting to database'))
+db.once('open', () => {
+  console.log('Connected to database.')
+})
+
+// Define db model.
+// ================
+var itemSchema = mongoose.Schema({
+  name: String,
+  status: Boolean
+})
+
+var Item = mongoose.model('Item', itemSchema)
+
+// var item = new Item
+// item.name = 'banana'
+// item.status = true
+// item.save(() => {
+//   console.log('Seeded database.')
+// })
+
+// Server stuff.
+// =============
 var port = 3000
 var host = '127.0.0.1'
 
@@ -25,6 +53,8 @@ app.listen(port, host, (request, response) => {
   console.log(`Listening on ${host}:${port}...`)
 })
 
+// Routing and handling.
+// =====================
 app.get('/', (request, response) => {
   response.render('index')
 })
